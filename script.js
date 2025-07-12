e85d21462af0c1a63743b744d505fe3ac832e193
Kuvarsinghrai <160009023+Kuvarsinghrai@users.noreply.github.com>
2025-07-12 09:01:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+// const ImageElement = document.querySelector(".image-container");
+
+// const prevEle=document.getElementById("prev");
+// const nextEle=document.getElementById("next");
+
+// let x=0;
+// let timer;
+
+// function UpdateGalary(){
+   
+//     //  ImageElement.style.transform=' perspective(1000px) rotateY(${x}deg)';
+    
+
+//      timer=setTimeout(()=>{
+//         x=x-45;
+//         UpdateGalary();
+//      },3000)
+// }
+
+// UpdateGalary();
+
+
+
+
+// const ImageElement = document.querySelector(".image-container"); 
+ 
+// const prevEle = document.getElementById("prev"); 
+// const nextEle = document.getElementById("next"); 
+ 
+// let x = 0; 
+// let timer; 
+ 
+// function updateGallery(){ 
+//      ImageElement.style.transform = 'perspective(1000px) rotateY(${x}deg)'; 
+ 
+//      timer = setTimeout(()=>{ 
+//         x = x - 45; 
+//         updateGallery(); 
+//      },3000) 
+// }  
+ 
+// updateGallery(); 
+ 
+// prevEle.addEventListener("click",()=>{ 
+//     x = x+45; 
+//     clearTimeout(timer); 
+//     updateGallery(); 
+// }) 
+ 
+ 
+// nextEle.addEventListener("click", () => { 
+//   x = x - 45; 
+//   clearTimeout(timer); 
+//   updateGallery(); 
+// });
+const ImageElement = document.querySelector(".image-container");
+const prevEle = document.getElementById("prev");
+const nextEle = document.getElementById("next");
+
+let x = 0;
+let timer;
+
+function updateGallery() {
+  ImageElement.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+
+  timer = setTimeout(() => {
+    x = x - 45;
+    updateGallery();
+  }, 3000);
+}
+
+updateGallery();
+
+prevEle.addEventListener("click", () => {
+  x = x + 45;
+  clearTimeout(timer);
+  updateGallery();
+});
+
+nextEle.addEventListener("click", () => {
+  x = x - 45;
+  clearTimeout(timer);
+  updateGallery();
+});
